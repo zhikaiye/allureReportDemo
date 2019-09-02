7f49863d7cdab1087833f6fcde0b90502bddb9cc
Zhikai Ye <zhikaiye939@gmail.com>
2019-09-02 21:24:50 +0800

    提交啊提交

---
+++ src/test/java/AlluerReportDemo.java
@@ -0,0 +1,16 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AlluerReportDemo {
+
+    @Test
+    public void testCase01(){
+        System.out.println("this is report");
+    }
+
+    @Test
+    public void testCase02(){
+        System.out.println("This is failure case");
+        Assert.assertEquals("1","2");
+    }
+}
